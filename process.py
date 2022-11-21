478ba368a63f5c545a8672c461b93335aba9a5a2
Dan Davison <dandavison7@gmail.com>
2022-11-20 21:50:53 -0500

    Add edge labels

--- process.py
+++ process.py
@@ -24,8 +24,10 @@ def make_tree(path: str) -> nx.DiGraph:
         parent = root
         nx.set_node_attributes(graph, {parent: parent.label}, 'label')
         for (state, prob) in zip(states, probs):
+            prob = float(prob)
             child = Node(f"{parent.path} -> {state}", state)
-            graph.add_edge(parent, child, prob=float(prob), name=state)
+            graph.add_edge(parent, child, prob=prob, name=state)
+            nx.set_edge_attributes(graph, {(parent, child): prob}, 'label')
             nx.set_node_attributes(graph, {child: child.label}, 'label')
             parent = child
 
